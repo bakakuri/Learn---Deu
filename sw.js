f225c86c6aeb990f9be67eef737f8703ff3f20e1
bakakuri <78099139+bakakuri@users.noreply.github.com>
2026-04-23 17:55:03 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = "deutschgeo-v3";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./css/style.css",
+  "./js/main.js",
+  "./json/vocabulary.json",
+  "./json/daily_phrases.json",
+  "./json/achievements.json",
+  "./manifest.json",
+  "./img/icon-192.png",
+  "./img/icon-512.png",
+  "https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;700;900&family=DM+Sans:wght@300;400;500;600&family=JetBrains+Mono:wght@400;500&display=swap"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      if (cached) return cached;
+      return fetch(e.request).then(res => {
+        if (!res || res.status !== 200 || res.type === "opaque") return res;
+        const clone = res.clone();
+        caches.open(CACHE_NAME).then(c => c.put(e.request, clone));
+        return res;
+      }).catch(() => cached);
+    })
+  );
+});
